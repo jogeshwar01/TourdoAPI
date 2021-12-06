a7fa4c30cce417639bcc06c454bf55d831a034b6
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-06 20:34:03 +0530

    add basic middleware

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-//middleware -it can modify incoming request data
+//1 .MIDDLEWARES
+app.use(morgan('dev'))
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware ✌️');
+    next();
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+
+//2. ROUTE HANDLERS
+
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
+
     res.status(200).json({
         //add status as we use JSend format       
         //added result just for easy info to client(not part of JSend)
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours            //no need to write both key and value if they have same name in ES6
@@ -107,6 +126,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+//3. ROUTES
+
 app.route('/api/v1/tours')
     .get(getAllTours)
     .post(createTour)
@@ -116,6 +137,8 @@ app.route('/api/v1/tours/:id')
     .patch(updateTour)
     .delete(deleteTour)
 
+
+//4. START THE SERVER
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
