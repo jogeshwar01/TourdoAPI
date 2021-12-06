825bd1850d9d9e5a8c5ec1bf33d21da4453a579a
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-06 13:19:22 +0530

    get tour by id (url parameters)

--- app.js
+++ app.js
@@ -54,6 +54,29 @@ app.post('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    //can add ? for optional parameters like :id?
+    console.log(req.params);
+
+    const id = req.params.id * 1;  //trick to convert string to number
+    const tour = tours.find(el => el.id == id);
+
+    // if (id > tours.length) {     //another alternative condition
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
