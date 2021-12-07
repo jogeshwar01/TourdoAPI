baba6c16e3e0d83feb0678b0f34f317a6c81168a
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-07 13:27:46 +0530

    add and mount multiple routers

--- app.js
+++ app.js
@@ -164,23 +164,30 @@ const deleteUser = (req, res) => {
 
 //3. ROUTES
 
-app.route('/api/v1/tours')
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/')
     .get(getAllTours)
     .post(createTour)
 
-app.route('/api/v1/tours/:id')
+tourRouter.route('/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour)
 
-app.route('/api/v1/users')
+userRouter.route('/')
     .get(getAllUsers)
     .post(createUser)
 
-app.route('/api/v1/userd/:id')
+userRouter.route('/:id')
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser)
+
+//mounting routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 //4. START THE SERVER
 const port = 3000;
