e61ed742ce6b17454a270dbb9b5c494bf5fdac30
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-08 18:24:25 +0530

    inbuilt middleware to serve static files

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 //MIDDLEWARES
 app.use(morgan('dev'))
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));     //public is default folder--to serve static files like any html or images
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware ✌️');
