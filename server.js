f40bd0028b8ef1c1af989c0d9a97ac690a894e84
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-09 13:30:38 +0530

    add and setup environment variables

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });    //read variables from config.env file and save them to node environment variables
+//can use these env variables using process anywhere and dont need to do anything now again
+
+//"start:prod": "SET NODE_ENV=production & nodemon server.js" --correct syntax in windows for npm start:prod
+
+//need to have this after configuring as we use it in app.js
 const app = require('./app');
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
