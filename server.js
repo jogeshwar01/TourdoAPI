757df1cf2720d4ef4b6523c5cedaa4382f4bd9b4
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-09 19:22:35 +0530

    connect atlas database to express app

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });    //read variables from config.env file and save them to node environment variables
 //can use these env variables using process anywhere and dont need to do anything now again
@@ -7,6 +9,19 @@ dotenv.config({ path: './config.env' });    //read variables from config.env fil
 //need to have this after configuring as we use it in app.js
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then((con) => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
