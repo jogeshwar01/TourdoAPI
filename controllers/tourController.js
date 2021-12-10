08ea6edf95f7b0d45a67f304f7fcb1a54b65c189
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-10 12:37:13 +0530

    delete documents with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,9 +86,19 @@ exports.updateTour = async (req, res) => {
     }
 }
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+exports.deleteTour = async (req, res) => {
+    try {
+        //in RESTful API ,we dont send the deleted object back to the client
+        await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
