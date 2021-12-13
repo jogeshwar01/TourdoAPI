715bfa3ab79784cb827e7b0c825affd861df0111
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-13 12:39:47 +0530

    start basic error handling

--- app.js
+++ app.js
@@ -25,9 +25,28 @@ app.use('/api/v1/users', userRouter);
 
 //to handle for all the requests/http methods.  -order matters hence this has to be at the last else all requests will go into this
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // })
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err); //next function with an argument is always taken as an error by express 
+    //and all other normal middlewares are skipped and we go to global error handling middleware
+});
+
+//express in built error handling middleware 
+//just give the four arguments so that express knows to run it only on errors
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;     //500 default for internal server error
+    err.status = err.status || 'error';     //for 500 status is error , for 404 it is fail
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     })
 });
 
