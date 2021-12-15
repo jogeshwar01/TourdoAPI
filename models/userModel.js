d06270abcaf9646617d5e72c2d17c2eb74af3def
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-15 12:36:30 +0530

    manage passwords - bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,8 +22,29 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // This only works on CREATE and SAVE!!!
+            // hence when we want to even update a user we would need to use save rather than findOneAndUpdate
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!'
+        }
     }
+});
+
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified/created new
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm field 
+    // passwordConfirm--required means it is required input but not that it needs to be persisted in the database
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
