ecd7f8f424d784383630d0d2e74a7a00a353202a
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-17 12:56:11 +0530

    set security http headers using helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+// Set security HTTP headers -put in beginning
+app.use(helmet());  // helemt() will return the desired middleware function
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -24,9 +29,13 @@ const limiter = rateLimit({
 // to see how many requests pending in postman check headers in output
 app.use('/api', limiter);   // limit our api routes
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));   //max amount of data that can come into body set to 10kb
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));     //public is default folder--to serve static files like any html or images
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     //console.log(req.headers);
