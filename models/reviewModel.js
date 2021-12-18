2514ca783ee22d8a4fa5c594210bf8244cc5d278
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-18 12:12:41 +0530

    model reviews - parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,42 @@
+// review / rating / createdAt / ref to tour / ref to user
+const mongoose = require('mongoose');
+const Tour = require('./tourModel');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review can not be empty!']
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        // PARENT REFERENCING
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.']
+        },
+        // PARENT REFERENCING
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user']
+        }
+    },
+    {
+        // So that virtual properties also shows up in json outputs and objects
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
