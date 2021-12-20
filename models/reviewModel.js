a788760aeedebde3af0904c735554135d8f9a549
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-20 13:49:40 +0530

    calc avg rating on tours based on create/update/delete on reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -55,6 +55,68 @@ reviewSchema.pre(/^find/, function (next) {
     next();
 });
 
+// STATIC METHODS IN MONGOOSE --these can be called on model directly like Review.calcAverageRatings
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    // aggregation pipeline -'this' points to model
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    //console.log(stats);  //eg output)- [ {_id: 61c0338f5a5b203d2cce2933, nRating: 3,avgRating: 4.666666666666667} ]
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
+};
+
+//Create reviews
+//need to use post and not presave as we need it to be saved in our collection to do calculation on all reviews
+reviewSchema.post('save', function () {
+    // this points to current review document being saved
+    // so we want to call our function using this.tour (so we use constructor to access tour)
+
+    //we cannot use Review here as it is declared after this 
+    //and we cannot shift it after it as then order matters and this fn will the not be seen
+    //so we use this.constructor where constructor is basically the model that created that document ie. Tour
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+// for updating and deleting we did using findOneByIdAndDelete so we cannot use document middleware
+// so we need to use QUERY MIDDLEWARE in which we dont have direct access to current review 
+// so we use this trick to do the same
+// we use findOneAnd hook here as findOneByIdAndDelete is shorthand for find One and delete for the current id
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();  //here 'this' is current query if if we execute it we get our document
+    //way of passing data from pre to post middleware
+    //saved it as this.r so that we can use this in the post middleware also
+
+    //console.log(this.r);      //will contain the non-updated data
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    // await this.findOne(); does NOT work here, query has already executed
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
